95d72b86d57082f7abd52ef383a9945e67feced7
nothing-c <halskdjf@gmail.com>
2024-09-03 20:45:57 -0500

    Get url fn fleshed out

--- crackboard.el
+++ crackboard.el
@@ -4,5 +4,30 @@
 (setq crackboard-heartbeat-interval (* 2 60 1000)) ;; 2m millis
 
 ;; Functions: one to start the daemon, one to send heartbeats
+(defun crackboard-send-heartbeat (l)
+  "Send heartbeat to crackboard.dev in language l"
+  (let ((url-request-method "POST")
+	(url-request-extra-headers
+	 '(("Content-Type" . "application/json")))
+	(url-request-data (json-encode '(("timestamp" . (format-time-string "%Y-%m-%dT%TZ" (current-time) t)) ;; Timestamp, Zulu
+		    ("session_key" . crackboard-session-key)
+		    ("language_name" . (crackboard-filetype)))))))
+    (url-retrieve crackboard-heartbeat-endpoint))
+
+(defun crackboard (k)
+  "Start crackboard session with key k"
+  (interactive "sKey: ")
+  )
+
+(defun crackboard-fmt (k l)
+  "Format key and language into timestamped JSON to send as heartbeat"
+  )
+
+(defun crackboard-filetype ()
+  "Locate filetype of current buffer (hacky rn)"
+
 
 ;; Functionality: track typing, track saving
+(json-encode '(("timestamp" . (format-time-string "%Y-%m-%dT%TZ" (current-time) t)) ;; Timestamp, Zulu
+	       ("session_key" . crackboard-session-key)
+	       ("language_name" . "foo")))
