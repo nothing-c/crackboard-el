1ee8c1dc8e029d43e86d913a6ebbbbaa74a4397a
nothing-c <halskdjf@gmail.com>
2024-09-03 20:57:17 -0500

    Add hooks

--- crackboard.el
+++ crackboard.el
@@ -14,19 +14,23 @@
 		    ("language_name" . (crackboard-filetype)))))))
     (url-retrieve crackboard-heartbeat-endpoint))
 
-(defun crackboard (k)
-  "Start crackboard session with key k"
-  (interactive "sKey: ")
-  (setq crackboard-session-key k)
-  ;; do something else
-  )
-
 (defun crackboard-filetype ()
   "Locate filetype of current buffer (hacky rn)"
   (replace-regexp-in-string "-mode$" "" (symbol-name major-mode)))
 
-
 ;; Functionality: track typing, track saving
-(json-encode '(("timestamp" . (format-time-string "%Y-%m-%dT%TZ" (current-time) t)) ;; Timestamp, Zulu
-	       ("session_key" . crackboard-session-key)
-	       ("language_name" . "foo")))
+(defun crackboard-change (b e l)
+  "Send heartbeat on buffer change. Beginning/end/len are passed to every function that uses this hook, so we take them because we have to."
+  (crackboard-send-heartbeat (crackboard-filetype)))
+
+(defun crackboard-save ()
+  "Send heartbeat on file save."
+  (crackboard-send-heartbeat (crackboard-filetype)))
+
+(defun crackboard (k)
+  "Start crackboard session with key k"
+  (interactive "sKey: ")
+  (setq crackboard-session-key k)
+  ;; add hooks
+  (add-hook 'after-save-hook 'crackboard-save)
+  (add-hook 'after-change-functions 'crackboard-change))
